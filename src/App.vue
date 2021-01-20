35e3896467dba574225232ccc22b6bf4469f4f31
Jorge Alberto Jasso Torres <jorge.jasso@thincode.com>
2021-01-19 19:01:04 -0600

    Add styles

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,14 @@
 <template>
   <div id="app">
-    <div v-for="film in films" :key="film.id">
+    <div class="headers">
+      <span>Episode</span>
+      <span>Title</span>
+      <span>Release Date</span>
+      <span>Director</span>
+      <span>Producer</span>
+      <span>Opening Crawl</span>
+    </div>
+    <div v-for="film in films" :key="film.id" class="film">
       <span>{{ film.episode_id }}</span>
       <span>{{ film.title }}</span>
       <span>{{ film.release_date }}</span>
@@ -33,8 +41,45 @@ export default {
       })
       .finally(() => {
         this.loading = false
-        
+
       })
   },
 }
 </script>
+
+<style scoped>
+  .film span,
+  .headers span {
+    display: inline-block;
+  }
+
+  .film span:nth-of-type(1),
+  .headers span:nth-of-type(1) {
+    width: 10%;
+  }
+
+  .film span:nth-of-type(2),
+  .headers span:nth-of-type(2) {
+    width: 10%;
+  }
+
+  .film span:nth-of-type(3),
+  .headers span:nth-of-type(3) {
+    width: 10%;
+  }
+
+  .film span:nth-of-type(4),
+  .headers span:nth-of-type(4) {
+    width: 10%;
+  }
+
+  .film span:nth-of-type(5),
+  .headers span:nth-of-type(5) {
+    width: 10%;
+  }
+
+  .film span:nth-of-type(6),
+  .headers span:nth-of-type(6) {
+    width: 50%;
+  }
+</style>
